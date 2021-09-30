936cc4c1a5d26721345e6c6dea14d1db0578d6f0
branfung <brandon.fung@upr.edu>
2021-09-30 02:23:23 -0400

    flask backend

---
+++ medical-appointments-team-N/api/server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/')
+def helloWorld():
+    return {'msg': 'Hello World !'}
