47e862c753bcbd800691fdeaa53b326d1904fc62
cmachin <carlos.machin@upr.edu>
2021-10-10 20:52:45 -0400

    show database tables

--- medical-appointments-team-N/database/MySQLconnector.py
+++ medical-appointments-team-N/database/MySQLconnector.py
@@ -3,11 +3,23 @@ import mysql.connector
 user = 'admin'
 password = 'Inso4116'
 host = 'inso-databse.c7sgroillrv6.us-east-2.rds.amazonaws.com'
-database = 'doctor_data'
+database = 'MedSearch'
 port = '3306'
 
 db = mysql.connector.connect(user=user, password=password, host=host, database=database, port=port)
 
 print("Connected to database!")
+
+cursor = db.cursor(buffered=True)
+
+query = ("SHOW Tables;")
+
+cursor.execute(query)
+
+tables = cursor.fetchall()
+
+print(tables)
+
+cursor.close()
 
 db.close()
